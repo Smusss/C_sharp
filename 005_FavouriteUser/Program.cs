5054bc0f7e8cc58bdb0b5fb7fc9b93d633090d3c
Smusss <lilly_dallas@bk.ru>
2022-10-16 20:02:36 +0300

    Add favourite user

---
+++ 005_FavouriteUser/Program.cs
@@ -0,0 +1,11 @@
+﻿// Say Hi to favourite User
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Здорова Машоооок!!!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
