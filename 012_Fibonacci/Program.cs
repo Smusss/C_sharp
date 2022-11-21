cff9ba0d68fb56249f09e0634bdddaedfdf1a58a
Smusss <lilly_dallas@bk.ru>
2022-11-21 14:41:10 +0300

    Add 012_fibonacci

---
+++ 012_Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿// фибоначчи и рекурсия
+
+int Fibo(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibo(n - 1) + Fibo(n - 2);
+    }
+}
+
+int num = 30;
+for (int i = 1; i < num; i++)
+{
+    Console.WriteLine($"{Fibo(i)} ");
+}
