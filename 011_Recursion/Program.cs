58fb26c6d3b97ca3d7213da5166f54940685e009
Smusss <lilly_dallas@bk.ru>
2022-11-21 12:50:09 +0300

    Add 011_recursion (factorial)

---
+++ 011_Recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double Factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+       return n * Factorial(n-1);
+    }
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console. WriteLine(Factorial(i));
+}
