0a00bdc24f9e75950495dd6c5498322496650014
Smusss <lilly_dallas@bk.ru>
2022-11-21 11:41:47 +0300

    Add 009_matrix

---
+++ 009_Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿// двухмерный массив - числа
+
+void printArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void fillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+fillArray(matrix);
+printArray(matrix);
